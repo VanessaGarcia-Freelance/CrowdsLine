cc72b7fd0c54b4580f854103ac581bd4f198de5d
Vanessa Garcia <me@vanessa-lyn.com>
2016-01-25 13:49:59 -0800

    setting up responsive settings for the latest posts slider

--- themes/TCL2016/js/tcl2016.js
+++ themes/TCL2016/js/tcl2016.js
@@ -55,7 +55,32 @@
         slidesToShow: 5,
         centerMode: true,
         prevArrow: '<button type="button" class="slick-prev"><</button>',
-        nextArrow: '<button type="button" class="slick-next">></button>'
+        nextArrow: '<button type="button" class="slick-next">></button>',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
 
     });
